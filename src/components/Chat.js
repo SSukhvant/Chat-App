92740ba26be791d1ed2764bb11f3b343c30cc035
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2022-03-28 16:28:41 +0530

    added sound

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,20 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { db, auth } from '../firebase'
-import Send from './Send'
-import SignOut from './SignOut'
+import React, { useState, useEffect, useRef } from 'react';
+import { db, auth } from '../firebase';
+import useSound from 'use-sound';
+import Send from './Send';
+import SignOut from './SignOut';
+import sent from '../sound/notification.mp3';
+import received from '../sound/recieved.mp3';
 
 function Chat() {
+    const [playOn] = useSound(
+        sent,
+        { volume: 0.25 }
+      );
+      const [playOff] = useSound(
+          received,
+        { volume: 0 }
+      );
     const scroll = useRef()
     const [messages, setMessages] = useState([])
     useEffect(() => {
@@ -18,9 +29,13 @@ function Chat() {
                 {messages.map(({ id, text, photoURL, uid }) => (
                     <div>
                         <div key={id} className={`msg ${uid === auth.currentUser.uid ? 'sent' : 'received'}`}>
-                            <img src={photoURL} alt="" />
+                            <img src={photoURL} alt="profile" />
                             <p>{text}</p>
+                            {
+                                uid === auth.currentUser.uid ? playOn() : playOff()
+                             }
                         </div>
+                     
                     </div>
                 ))}
             </div>
